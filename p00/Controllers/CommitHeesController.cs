021957dc626d1ce011696b334a9afd83383b762e
othman shaker <athman1937@gmail.com>
2020-04-19 08:15:10 +0300

    EditAddcommheemembers

--- p00/Controllers/CommitHeesController.cs
+++ p00/Controllers/CommitHeesController.cs
@@ -42,7 +42,7 @@ namespace p00.Controllers
             var list1 = new List<string>() { "مؤقته", "دائما" };
             ViewBag.list1 = list1;
             var list2 = new List<string>() { "كيان", "لجنه" };
-            ViewBag.list2 = list1;
+            ViewBag.list2 = list2;
             return View();
         }
 
